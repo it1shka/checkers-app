11ea0a13b49311366647f0a91c2325a5e6462146
it1shka <2tishbel@gmail.com>
2024-12-02 23:00:43 +0100

    wip(tests): added another test for board

--- app/src/test/java/com/it1shka/checkers/GamelogicBoardPlayTest.kt
+++ app/src/test/java/com/it1shka/checkers/GamelogicBoardPlayTest.kt
@@ -7,7 +7,7 @@ import org.junit.Assert.*
 
 class BoardPlayTest {
   @Test
-  fun `should be able to have a basic game`() {
+  fun `should be able to handle a basic game`() {
     val moves = listOf(
       21 to 17,
       10 to 14,
@@ -38,6 +38,45 @@ class BoardPlayTest {
       "* r * * ",
       " * * * *",
       "* b b * ",
+      " b b b b",
+      "b b b b ",
+      "Turn: RED"
+    ).joinToString("\n")
+    assertEquals(expected, board.toString())
+  }
+
+  @Test
+  fun `should handle a simple game #2`() {
+    val moves = listOf(
+      22 to 18,
+      12 to 16,
+      24 to 20,
+      11 to 15,
+      20 to 11,
+      7 to 16,
+      18 to 11,
+    )
+    var board = Board.new();
+    for (maybeMove in moves) {
+      val move = maybeMove.intPairAsMove
+      if (move == null) {
+        fail("$maybeMove should be a valid move")
+        continue
+      }
+      val maybeNextBoard = board.makeMove(move)
+      if (maybeNextBoard == null) {
+        fail("$move failed")
+        continue
+      }
+      board = maybeNextBoard
+    }
+    val expected = listOf(
+      " r r r r",
+      "r r * r ",
+      " r r b *",
+      "* * * r ",
+      " * * * *",
+      "b * b * ",
       " b b b b",
       "b b b b ",
       "Turn: RED"
